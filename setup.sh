27dfb0787e72101b5468c08813f44bf51f33f69c
Mahmoud khalil <khalil2535@protonmail.com>
2020-02-28 00:45:18 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+# This script mession is to make the fisrt setup to the centos server as i wish
+
+dnf update # update the system because why not :p
+
+complete -c dnf -w yum # solve dnf auto complete problem
+
