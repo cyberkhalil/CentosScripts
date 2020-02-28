c2adf9a9ef92c1ceae018a82baae18373e3a101d
Mahmoud khalil <khalil2535@protonmail.com>
2020-02-28 10:38:09 +0200

    fix setup.sh issue

--- setup.sh
+++ setup.sh
@@ -1,21 +1,25 @@
+#!/bin/bash
 # This script mession is to make the fisrt setup to the centos server as i wish
 
 # function ask to yes no questions
-ask(){ return $(whiptail --yesno $1 20 60 3>&2 2>&1 1>&3);}
+ask(){ $(whiptail --yesno "$1" 20 60 3>&2 2>&1 1>&3); return $?;}
 
- update the system because why not :p
-if (ask "Do you want to update the system ?"); then
+#update the system because why not :p
+ask "Do you want to update the system ?"
+if [ $? == 0 ]; then
     echo "Updating the system"
     dnf update -q
 fi
 
- solve dnf auto complete problem
-if (ask "Do you want to solve dnf completion problem ?"); then
+# solve dnf auto complete problem
+ask "Do you want to solve dnf completion problem ?"
+if [ $? == 0 ]; then
     echo "enable auto complete for dnf"
     complete -c dnf -w yum
 fi
 
-if (ask "Do you want to install vbox guests ?"); then
+ask "Do you want to install vbox guests ?"
+if [ $? == 0 ]; then
     dnf -y install dkms bzip2 # install vbox guest requirements
     if (ask "Insert vbox guest iso (yes if you did, no if you didn't)"); then # request insert virtualbox guest iso
         mkdir /tmp/vbiso/
