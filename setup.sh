4b2622ac130538b263180d19efe23cf5b5ce105b
Mahmoud khalil <khalil2535@protonmail.com>
2020-02-28 11:58:43 +0200

    Update setup.sh (#1)
    * update no question
    
    * yes and quiet
    
    * umount disk after end
    
    * fix

--- setup.sh
+++ setup.sh
@@ -6,24 +6,25 @@ ask(){ $(whiptail --yesno "$1" 20 60 3>&2 2>&1 1>&3); return $?;}
 
 #update the system because why not :p
 ask "Do you want to update the system ?"
-if [ $? == 0 ]; then
+if [ $? = 0 ]; then
     echo "Updating the system"
-    dnf update -q
+    dnf -yq update
 fi
 
 # solve dnf auto complete problem
 ask "Do you want to solve dnf completion problem ?"
-if [ $? == 0 ]; then
+if [ $? = 0 ]; then
     echo "enable auto complete for dnf"
     complete -c dnf -w yum
 fi
 
 ask "Do you want to install vbox guests ?"
-if [ $? == 0 ]; then
-    dnf -y install dkms bzip2 # install vbox guest requirements
+if [ $? = 0 ]; then
+    dnf -yq install dkms bzip2 # install vbox guest requirements
     if (ask "Insert vbox guest iso (yes if you did, no if you didn't)"); then # request insert virtualbox guest iso
         mkdir /tmp/vbiso/
         mount /dev/cdroom /tmp/vbiso
         ./tmp/vbiso/VBoxLinuxAdditions.run
+        umount /dev/cdroom /tmp/vbiso
     fi
 fi
