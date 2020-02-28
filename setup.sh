a5cc8cb9795daee3e3fe12d81176d284d88d795e
Mahmoud khalil <khalil2535@protonmail.com>
2020-02-28 08:15:10 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,24 @@
 # This script mession is to make the fisrt setup to the centos server as i wish
 
-dnf update -q # update the system because why not :p
+ask(){ return whiptail --yesno $1 20 60;}
 
-complete -c dnf -w yum # solve dnf auto complete problem
+# update the system because why not :p
+if [ ask "Do you want to update the system ?" ]; then
+    echo "Updating the system"
+    dnf update -q
+fi
 
+# solve dnf auto complete problem
+if [ ask "Do you want to solve dnf completion problem ?" ]; then
+    echo "enable auto complete for dnf"
+    complete -c dnf -w yum
+fi
+
+if [ ask "Do you want to install vbox guests ?" ]; then
+    dnf -y install dkms bzip2 # install vbox guest requirements
+    if [ ask "Insert vbox guest iso (yes if you did, no if you didn't)" ]; then # request insert virtualbox guest iso
+        mkdir /tmp/vbiso/
+        mount /dev/cdroom /tmp/vbiso
+        ./tmp/vbiso/VBoxLinuxAdditions.run
+    fi
+fi
